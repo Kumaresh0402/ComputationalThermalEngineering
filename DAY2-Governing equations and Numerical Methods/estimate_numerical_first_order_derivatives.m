85bd4f7443047c7c8777c223273fd74ca8c7f144
Kumaresh0402 <143979398+Kumaresh0402@users.noreply.github.com>
2023-09-21 17:21:13 +0900

    Update estimate_numerical_first_order_derivatives.m
    Updated by comparing analytical and numerical (upwind and central difference) solutions

--- DAY2-Governing equations and Numerical Methods/estimate_numerical_first_order_derivatives.m
+++ DAY2-Governing equations and Numerical Methods/estimate_numerical_first_order_derivatives.m
@@ -19,19 +19,23 @@ set(gca, "linewidth", 2, "fontsize", 14)
 % gradient
 yp = 2*x; % Analytical expression
 
-yp_n1 = zeros(size(y));
+yp_upwind = zeros(size(y));
+yp_central = zeros(size(y));
 
 %yp_n1(1, 1) = (y(2, 1) - y(1, 1)) / (x(2, 1) - x(1, 1));
 %yp_n1(n, 1) = (y(n, 1) - y(n-1, 1)) / (x(n, 1) - x(n-1, 1));
 
-for i = 1 : length(y)-1
-  yp_n1(i, 1) = (y(i+1, 1) - y(i, 1)) / (x(i+1, 1) - x(i, 1));
+for i = 2 : length(y)-1
+  yp_upwind(i, 1) = (y(i+1, 1) - y(i, 1)) / (x(i+1, 1) - x(i, 1));
+  yp_central(i, 1) = (y(i+1, 1) - y(i-1, 1)) / (x(i+1, 1) - x(i-1, 1));
 end
 
 figure(2);
 hold on;
-plot(x(2:n-1), yp(2:n-1), '-ob', 'linewidth', 2);
-plot(x(2:n-1), yp_n1(2:n-1), '--om', 'linewidth', 2);
+plot(x(2:n-1), yp(2:n-1), '-b', 'linewidth', 2);
+plot(x(2:n-1), yp_upwind(2:n-1), '--om', 'linewidth', 2);
+plot(x(2:n-1), yp_central(2:n-1), 'ov', 'linewidth', 2);
+legend('analytical', 'upwind', 'centraldifference', 'location', 'northwest');
 hold on;
 xlabel('x');
 ylabel('d\rho/dx=2x');
