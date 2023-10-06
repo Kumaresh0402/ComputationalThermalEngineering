9d8775bd552180f0b643bff4dcbddbc0644881f3
Kumaresh0402 <143979398+Kumaresh0402@users.noreply.github.com>
2023-10-06 15:04:57 +0900

    Update 5_sampleC.cpp

--- DAY5-Rate of convergence and Introduction to C language/5_sampleC.cpp
+++ DAY5-Rate of convergence and Introduction to C language/5_sampleC.cpp
@@ -2,7 +2,13 @@
 
 int main()
 {
-    std::string openfoam = "OpenFOAM";
+    //std::string openfoam = "OpenFOAM";
 
-    std::cout << "Hello " << openfoam << std::endl;
+    //std::cout << "Hello openfoam\n";
+    //std::cout << "Hello " << openfoam << std::endl;
+    std::cout << "Hello openfoam" << std::endl;
+
+    // The command works from right end, "redirecting" to "Hello openfoam" to "std::cout" (print the statement in C language)
+    // "std::cout" --> "cout" is the function or class inside the namespace "std"
+    // namespace can access function or class using "::" --> name space resolution operator
 }
