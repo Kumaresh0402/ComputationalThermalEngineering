6465a9659e3753c59ec56c7e4709ee8dd5a38b47
Kumaresh0402 <143979398+Kumaresh0402@users.noreply.github.com>
2023-10-17 10:41:27 +0900

    Update a7_solve_diffusion_sample.m

--- DAY6-Diffusion equation and Introduction to C language/a7_solve_diffusion_sample.m
+++ DAY6-Diffusion equation and Introduction to C language/a7_solve_diffusion_sample.m
@@ -4,38 +4,47 @@ clear all;
 close all;
 
 % Create 1D grid.
-dx = ;
+dx = 0.05;
 x = [0:dx:1]';
 
 % Get the length of the array.
 n = length(x);
 
-alpha = ;
+alpha = 1;
 
 % Time control.
 t_initial = 0;
-t_final = 0.2; %1e-4;
-dt = ; % dx^2 / (2 * alpha); Von Neumann stability analysis - limitation for explicit Euler scheme
-% Stability is determined
+t_final = 0.05; %1e-4;
+dt = 1e-3; % dx^2 / (2 * alpha);
+%dt = 0.01; % dx^2 / (2 * alpha);
+%dt = 0.1; % dx^2 / (2 * alpha);
+
+r = alpha*dt / dx^2;
 
 % Boundary conditions.
-T_left = ;
-T_right = ;
+T_left = 300;
+T_right = 273; % Dirichlet BC
 
 T_initial = 350 * ones(n, 1);
 T_numerical = T_initial;
 
 % Set boundary conditions.
-T_numerical(1, 1) = T_left;
-T_numerical(n, 1) = T_right;
+% left boundary condition
+T_numerical(1, 1) = T_left;  %(i, 1) --> i = 1 % Dirichlet boundary condition
 
-count = 1;
+% Right boundary condition
+T_numerical(n, 1) = T_right; %- Dirichlet bounday condition  %(n, 1) --> i = n
+%T_numerical(n, 1) = T_numerical(n-1, 1); %% dT/dx = 0 (Neumann - adiabatic condition)
+
+count = 1;  % at step 1
+%count = 2;  % at step 2
 % Solve diffusion equation.
 for t = t_initial : dt : t_final
+    % t = 0: 0.001: 0.05
   fprintf('Time: %.3e\n', t);
   T_old = T_numerical;
   for i = 2 : n-1
-    T_numerical(i, 1) =
+    T_numerical(i, 1) = T_old(i, 1) + dt * alpha * (T_old(i+1, 1) - 2*T_old(i, 1) + T_old(i-1, 1)) / dx^2;
   endfor
 
   % Plot u.
